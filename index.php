e8343f1b609a8ae45642485daaef52aa75413862
Wara Massa <waramassa70@gmail.com>
2020-11-24 23:13:21 +0100

    php3 exo2

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "décembre"];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP3 exo2</title>
+</head>
+<body>
+    <?php
+    echo $months [5]
+    ?>
+    
+</body>
+</html>
